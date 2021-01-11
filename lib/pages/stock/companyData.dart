c96efe3973271b9b6858014d7e28e5d6ca7298a2
crebro <creationduwal@gmail.com>
2021-01-11 07:45:10 +0545

    Shown the user

--- lib/pages/stock/companyData.dart
+++ lib/pages/stock/companyData.dart
@@ -1,4 +1,5 @@
 import 'package:NewsForest/models/stockData.dart';
+import 'package:velocity_x/velocity_x.dart';
 import 'package:flutter/material.dart';
 
 class CompanyDataPage extends StatelessWidget {
@@ -14,9 +15,67 @@ class CompanyDataPage extends StatelessWidget {
       body: Container(
         margin: EdgeInsets.all(20),
         child: Column(
-          children: [],
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    stockData.maxPrice.text
+                        .color(this.getTradeColor())
+                        .xl5
+                        .make(),
+                    Icon(
+                      this.isPositiveTrade()
+                          ? Icons.keyboard_arrow_up
+                          : Icons.keyboard_arrow_down,
+                      color: this.getTradeColor(),
+                      size: 50,
+                    ),
+                  ],
+                ),
+                stockData.difference.text
+                    .color(this.getTradeColor())
+                    .xl3
+                    .make(),
+              ],
+            ),
+            buildValue("High", this.stockData.maxPrice),
+            buildValue("Low", this.stockData.minPrice),
+            buildValue("Traded Quantity", this.stockData.tradedShares),
+            buildValue("Total Trades", this.stockData.numberOfTransactions),
+            buildValue("Previous Closing ", this.stockData.previousClosing),
+            buildValue("Closing Price", this.stockData.closingPrice),
+          ],
         ),
       ),
     );
+  }
+
+  Widget buildValue(
+    String name,
+    dynamic value,
+  ) {
+    return RichText(
+        text: TextSpan(
+            children: [
+          TextSpan(
+              text: value.toString(), style: TextStyle(color: Colors.grey[700]))
+        ],
+            text: "$name: ",
+            style: TextStyle(fontSize: 30, color: Color(0xfff7921c))));
+  }
+
+  bool isPositiveTrade() {
+    var number = stockData.difference;
+    if (number >= 0) {
+      return true;
+    }
+    return false;
+  }
+
+  Color getTradeColor() {
+    return this.isPositiveTrade() ? Colors.green : Colors.red;
   }
 }
