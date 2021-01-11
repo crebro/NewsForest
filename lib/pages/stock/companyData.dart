26206a2d85db9af40aaae64c5959892c0fea5e82
crebro <creationduwal@gmail.com>
2021-01-11 06:58:06 +0545

    Implemented the Search system in Stock Exchange Data (Banks)

--- lib/pages/stock/companyData.dart
+++ lib/pages/stock/companyData.dart
@@ -11,7 +11,12 @@ class CompanyDataPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(stockData.name),
       ),
-      // body: ,
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          children: [],
+        ),
+      ),
     );
   }
 }
--- lib/pages/stock/stockExchangeData.dart
+++ lib/pages/stock/stockExchangeData.dart
@@ -11,10 +11,11 @@ class StockExchangeData extends StatefulWidget {
 class _StockExchangeDataState extends State<StockExchangeData> {
   List<StockData> stockData = [];
   ApiExtractor apiExtractor = ApiExtractor();
+  String searchQuery = "";
+  bool searching = false;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getStockData();
   }
@@ -36,11 +37,53 @@ class _StockExchangeDataState extends State<StockExchangeData> {
       appBar: AppBar(
         title: Text("NEPSE Data"),
       ),
-      body: ListView.builder(
-          itemCount: stockData.length,
-          itemBuilder: (context, index) {
-            return StockDataItem(stockData: stockData[index]);
-          }),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 15),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  if (value != "") {
+                    this.searchQuery = value;
+                    if (!this.searching) {
+                      this.searching = true;
+                    }
+                  } else {
+                    this.searching = false;
+                  }
+                });
+              },
+              decoration: InputDecoration(hintText: "Search For Banks"),
+            ),
+          ),
+          !this.searching
+              ? Expanded(
+                  child: ListView.builder(
+                      itemCount: stockData.length,
+                      itemBuilder: (context, index) {
+                        return StockDataItem(stockData: stockData[index]);
+                      }),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                      itemCount: this
+                          .stockData
+                          .where((element) => element.name
+                              .toLowerCase()
+                              .contains(this.searchQuery.toLowerCase()))
+                          .length,
+                      itemBuilder: (context, index) {
+                        List<StockData> items = this
+                            .stockData
+                            .where((element) => element.name
+                                .toLowerCase()
+                                .contains(this.searchQuery.toLowerCase()))
+                            .toList();
+                        return StockDataItem(stockData: items[index]);
+                      }))
+        ],
+      ),
     );
   }
 }
