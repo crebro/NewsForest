49d3dc5c352c1cc2e6b299aa81c4744ebe768950
crebro <creationduwal@gmail.com>
2021-01-12 08:49:42 +0545

    Changed the request location from localhost to herokuapp

--- lib/constants/apiConstants.dart
+++ lib/constants/apiConstants.dart
@@ -1,2 +1,1 @@
-//String requestLocation = "https://newsforestapi.herokuapp.com/";
-String requestLocation = "http://10.0.2.2:5000";
+String requestLocation = "https://newsforestapi.herokuapp.com/";
